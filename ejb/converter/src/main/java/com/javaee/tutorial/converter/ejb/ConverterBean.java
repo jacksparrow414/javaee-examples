86214458706b21e2a505c989fab23ccc3597c080
duhongbo <hongbo.du@etix.com>
2021-08-13 10:52:11 +0800

    ejb、jaxrs demo

---
+++ ejb/converter/src/main/java/com/javaee/tutorial/converter/ejb/ConverterBean.java
@@ -0,0 +1,20 @@
+package com.javaee.tutorial.converter.ejb;
+
+import javax.ejb.Stateless;
+import java.math.BigDecimal;
+
+@Stateless
+public class ConverterBean {
+    private final BigDecimal yenRate = new BigDecimal("104.34");
+    private final BigDecimal euroRate = new BigDecimal("0.007");
+
+    public BigDecimal dollarToYen(BigDecimal dollars) {
+        BigDecimal result = dollars.multiply(yenRate);
+        return result.setScale(2, BigDecimal.ROUND_UP);
+    }
+
+    public BigDecimal yenToEuro(BigDecimal yen) {
+        BigDecimal result = yen.multiply(euroRate);
+        return result.setScale(2, BigDecimal.ROUND_UP);
+    }
+}
---
+++ jaxrs/hello/src/main/java/com/javaee/tutorial/hello/HelloApplication.java
@@ -0,0 +1,8 @@
+package com.javaee.tutorial.hello;
+
+import javax.ws.rs.ApplicationPath;
+import javax.ws.rs.core.Application;
+
+@ApplicationPath(value = "resources")
+public class HelloApplication extends Application {
+}
